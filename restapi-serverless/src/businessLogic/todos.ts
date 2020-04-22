ec73c09d370361f45bb38d10517e881fb423a571
pullmana8 <pullmana8@gmail.com>
2020-04-22 14:35:26 -0500

    Add permissions

--- restapi-serverless/src/businessLogic/todos.ts
+++ restapi-serverless/src/businessLogic/todos.ts
@@ -5,14 +5,17 @@ import * as uuid from 'uuid'
 
 const todosAccess = new TodosAccess()
 
+/* Get all todo items */
+export async function getAllTodos(userId: string): Promise<TodoItem[]> {
+    return await getAllTodos(userId)
+}
+
+/* Create Todo Section */
 export async function createTodo(payload: CreateTodoRequest): Promise<TodoItem> {
-
     const todoId = uuid.v4()
-
     const data = {
         todoId,
         ...payload
     }
-
     return await todosAccess.createTodo(data)
 }
--- restapi-serverless/src/dataLayer/todosAccess.ts
+++ restapi-serverless/src/dataLayer/todosAccess.ts
@@ -6,6 +6,23 @@ export class TodosAccess {
         private readonly docClient: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient(),
         private readonly todosTable = process.env.TODOS_TABLE) { }
 
+    /* Get all todo items */
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+        console.log('Getting all todo items')
+
+        const params = {
+            TableName: this.todosTable,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            },
+            ScanIndexForward: false
+        }
+        const result = await this.docClient.query(params).promise()
+        return result.Items as TodoItem[]
+    }
+    
+    /* Create Todo Section */
     async createTodo(todo: TodoItem): Promise<TodoItem> {
         await this.docClient
             .put({
