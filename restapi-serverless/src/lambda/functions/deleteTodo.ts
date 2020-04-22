0bc924377e2ec1d002db0637269aa53ec191efab
pullmana8 <pullmana8@gmail.com>
2020-04-22 10:27:08 -0500

    Moved files to one folder

--- restapi-serverless/src/lambda/functions/deleteTodo.ts
+++ restapi-serverless/src/lambda/functions/deleteTodo.ts
@@ -4,6 +4,10 @@ import 'source-map-support/register'
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
         body: JSON.stringify({
             message: 'Delete all todo items',
             input: event,
