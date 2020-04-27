5b43bb9b64bab6a271579486fc4bdf62c7163018
pullmana8 <pullmana8@gmail.com>
2020-04-27 10:36:32 -0500

    Removed deleted files and import from the loadTodos

--- restapi-serverless/src/lambda/functions/createTodo.ts
+++ restapi-serverless/src/lambda/functions/createTodo.ts
@@ -7,19 +7,18 @@ import "source-map-support/register";
 import { createLogger } from "../../helpers/utils/logger";
 import { CreateTodoRequest } from "../../models/requests/createTodoRequest";
 import { getUserId } from "../authorization/token/lambdaUtils";
-import { createTodo } from "../../businessLogic/todosAccess";
+import { LoadTodos } from "../../dataLayer/loadTodos";
 
 const logger = createLogger('todos');
 
 export const handler: APIGatewayProxyHandler = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-    logger.info(`Processing creating todos: ${event}`)
-
     const newTodo: CreateTodoRequest = typeof event.body === "string" ? JSON.parse(event.body) : event.body
     const userId = getUserId(event)
-    const item = await createTodo(userId, newTodo)
-
+    logger.info(`Processing creating todos: ${event}`)
+    
+    const item = await new LoadTodos().createTodo(newTodo, userId)
     return {
         statusCode: 201,
         headers: {
